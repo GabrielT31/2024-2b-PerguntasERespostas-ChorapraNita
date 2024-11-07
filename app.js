9e3c32e01825073bb3360da37475f01d50009cfb
Gabriel Henrique Rodrigues Teixeira <159148600+GabrielT31@users.noreply.github.com>
2024-11-07 08:43:54 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 function criaCartao(Materia, Pergunta, Resposta){
-    let caixa = document.getElementsById('caixa')
+    let caixa = document.getElementById('caixa')
     let cartao = document.createElement('article')
 
     cartao.className = 'cartao'
 
-    cartao.innerHTML = `
-    <div class="cartao__conteudo">
+    cartao.innerHTML = `<div class="cartao__conteudo">
           <h3> ${Materia} </h3>
           <div class="cartao__conteudo__pergunta">
             <p>${Pergunta}
