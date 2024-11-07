f4c04b2415f8c6d0b5a765b0b0e58e3945e52a8c
Gabriel Henrique Rodrigues Teixeira <159148600+GabrielT31@users.noreply.github.com>
2024-11-07 08:35:18 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+function criaCartao(Materia, Pergunta, Resposta){
+    let caixa = document.getElementsById('caixa')
+    let cartao = document.createElement('article')
+
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+          <h3> ${Materia} </h3>
+          <div class="cartao__conteudo__pergunta">
+            <p>${Pergunta}
+            </p>
+          </div>
+          <div class="cartao__conteudo__resposta">
+            <p>${Resposta}</p>
+          </div>
+          `
+
+          caixa.appendChild(cartao)
+}
---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    "Matemática",
+    "Uma mulher irá ter um bebê. Se ele for menino, ficará faltando apenas um filho para que o número de meninos seja igual ao de filhas. No entanto, se o bebê for uma menina, o número de filhas da mulher será dobro do número de meninos. Quantos filhos ela tem e qual é o sexo deles?",
+    "A mulher tem 8 filhos - 3 meninos e 5 meninas."
+)
+criaCartao(
+    "Geografia",
+    "Capital do estado de Rondônia",
+    "Porto Velho."
+)
+criaCartao(
+    "Física",
+    "Um termômetro digital, localizado em uma praça na Inglaterra, marca a temperatura de 10,4 ºF. E temperatura, na escala Celsius, corresponde a?",
+    "12 ºC"
+)
+criaCartao(
+    "Matemática",
+    "Qual o próximo número na sequência: 2, 10, 12, 16, 17, 18, 19, ...?",
+    "200"
+)
+criaCartao(
+    "Geografia",
+    "A Geografia é a ciência",
+    "se preocupa em compreender os aspectos e as dinâmicas do espaço geográfico"
+)
+criaCartao(
+    "Física",
+    "A chegada do homem a lua aconteceu em 1969. A estrutura da lua é rochosa e praticamente não possui atmosfera, o que faz com que durante o dia a temperatura chegue a 105 °C e à noite caia para −155 °C. Essa variação térmica, medida na escala Fahrenheit de temperatura, vale",
+    "468"
+)
